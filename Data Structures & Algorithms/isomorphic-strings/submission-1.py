9e5e1d6da9d94bb3c89a425cf199058e1ea783a2
sridharkamath <73011349+sridharkamath@users.noreply.github.com>
2026-06-03 23:18:51 +0530

    Add: isomorphic-strings - submission-1

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(set(s))!=len(set(t)):
+            return False
+        else:
+            m={}
+            for i in range(len(t)):
+                if s[i] not in m:
+                    s=s.replace(s[i],t[i])
+                    m[s[i]]=t[i]
+            return s==t   
